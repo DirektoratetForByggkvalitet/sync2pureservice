d1a083226d60a7546ca942efdb8249b8fd3f0f85
Eirik Wulff <ew@dibk.no>
2023-04-04 10:17:19 +0200

    Kjører nå fra standard php:fpm-alpine

---
+++ scripts/php-install-alpine.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Nødvendige pakker
+apk add --no-cache curl tar openssl xz
+
+# Installerer PHP-extensions med https://github.com/mlocati/docker-php-extension-installer
+curl -Lso /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions
+chmod +x /usr/local/bin/install-php-extensions
+install-php-extensions opcache @composer
