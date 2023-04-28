4b997e8f9893165a5c13531755a65609a11f08b7
Eirik Wulff <ew@dibk.no>
2023-04-28 15:10:27 +0200

    Tar høyde for manglende orgnr

--- app/Console/Commands/SvarInn2Pureservice.php
+++ app/Console/Commands/SvarInn2Pureservice.php
@@ -93,7 +93,7 @@ class SvarInn2Pureservice extends Command {
                      * Hvordan løse problematikk når e-postadresse ikke finnes?
                      * Vi oppretter en falsk e-postadresse basert på orgnr. som brukes til å lage bruker.
                      */
-                    $email = Arr::get($message,'svarSendesTil.orgnr').'.no_email@pureservice.local';
+                    $email = Arr::get($message,'svarSendesTil.orgnr') != null ? Arr::get($message,'svarSendesTil.orgnr').'.no_email@pureservice.local' : Str::random(9).'.no_email@pureservice.local';
                 endif;
                 if ($userInfo = $this->ps->findUser($email)):
                     $this->line($this->l3.'Foretaksbruker er registrert i Pureservice');
