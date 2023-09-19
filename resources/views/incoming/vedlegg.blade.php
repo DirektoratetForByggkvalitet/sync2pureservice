da6bec778d3ce57e05dcfb282eec65709a01b4b7
Eirik Wulff <ew@dibk.no>
2023-09-19 11:34:49 +0200

    Tar vekk arkivmelding.xml fra vedleggslisten

--- resources/views/incoming/vedlegg.blade.php
+++ resources/views/incoming/vedlegg.blade.php
@@ -4,7 +4,14 @@
 <h2>{{ $subject }}</h2>
 <ul>
 @foreach ($attachments as $filepath)
-    <li>{{ Str::afterLast($filepath, '/') }}</li>
+
+    @php
+        $filename = Str::afterLast($filepath, '/');
+        // Hopper over arkivmelding.xml
+        if ($filename == 'arkivmelding.xml') continue;
+    @endphp
+
+    <li>{{ $filename }}</li>
 
 @endforeach
 </ul>
