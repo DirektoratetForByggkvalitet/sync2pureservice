6070ffe894f5edf2cb00175d55f7f5960e4f1d8c
Eirik Wulff <ew@dibk.no>
2023-10-20 08:39:10 +0200

    Mail: endret på oppsettet for failover. Prioriterer å bruke Microsoft Graph, deretter smtp

--- config/mail.php
+++ config/mail.php
@@ -96,8 +96,8 @@ return [
         'failover' => [
             'transport' => 'failover',
             'mailers' => [
-                'smtp',
                 'microsoft-graph',
+                'smtp',
             ],
         ],
     ],
