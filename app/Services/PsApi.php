d32770a0646a0d53120d08fbba73ff232649431c
Eirik Wulff <ew@dibk.no>
2024-01-30 06:55:15 +0100

    PsApi: Hentet inn disableCompanyOrUser fra schedules

--- app/Services/PsApi.php
+++ app/Services/PsApi.php
@@ -932,4 +932,11 @@ class PsApi extends API {
         $response = $this->apiPatch($uri, $body, 'application/json', '*/*');
         return $response->successful();
     }
+
+    public function disableCompanyOrUser(User|Company $object): void {
+        $uri = $object instanceof User ? '/user/'.$object->id : '/company/'.$object->id;
+        $body = ['disabled' => true];
+        $response = $this->apiPatch($uri, $body, 'application/json');
+    }
+
 }
