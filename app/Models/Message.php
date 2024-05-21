85626bdcd7d5caab61ebe53730a7156c65f09918
Eirik Wulff <ew@dibk.no>
2024-05-21 07:34:58 +0200

    WIP: Jobber med å legge inn vedlegg ved oppretting av saken

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -281,7 +281,7 @@ class Message extends Model {
             $description = Blade::render(config('eformidling.in.arkivmelding'), ['subject' => $subject, 'msg' => $this]);
         //dd($description);
         endif;
-        if ($ticket = $ps->createTicket($subject, $description, $senderUser->id, config('pureservice.visibility.invisible'))):
+        if ($ticket = $ps->createTicket($subject, $description, $senderUser->id, config('pureservice.visibility.invisible'), )):
             if (count($this->attachments) && $addAttachments):
                 // Oppretter en kommunikasjon med vedleggene som vedlegg
                 $ps->addCommunicationToTicket($ticket, $senderUser->id, $this->attachments);
