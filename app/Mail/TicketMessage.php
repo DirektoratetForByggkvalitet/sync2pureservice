b69c8278ea2809950d33b6bacfed229adb09e5f7
Eirik Wulff <ew@dibk.no>
2024-08-27 14:42:56 +0200

    Manglende innstillinger lagt til

---
+++ app/Mail/TicketMessage.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\{Content, Envelope, Attachment};
+use Illuminate\Queue\SerializesModels;
+use App\Models\Ticket;
+
+class TicketMessage extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Oppretter en ny e-postmelding fra en sak
+     * Det er også mulig å oppgi
+     * $subject, $content og $attachments for å overstyre saksdataene
+     */
+    public function __construct(
+        public Ticket|null $ticket = null,
+        public bool $includeFonts = false,
+        public string|null $title = null,
+        public string|null $contents = null,
+        public array|null $attachFiles = null,
+    ) {}
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope {
+        return new Envelope(
+            subject: $this->title ? $this->title : $this->ticket->emailSubject()
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content {
+        return new Content(
+            view: $this->contents ? 'rawmessage' : 'message'
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array {
+        $attachments = [];
+        if (is_array($this->attachFiles)):
+            $attachmentArray = $this->attachFiles;
+        elseif ($this->ticket && is_array($this->ticket->attachments)):
+            $attachmentArray = $this->ticket->attachments;
+        else:
+            return $attachments;
+        endif;
+
+        if (is_array($attachmentArray) && count($attachmentArray)):
+            foreach ($attachmentArray as $file):
+                $attachments[] = Attachment::fromStorage($file);
+            endforeach;
+        endif;
+        return $attachments;
+    }
+}
