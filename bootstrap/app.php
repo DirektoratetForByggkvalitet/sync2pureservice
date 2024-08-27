b06588aaf917669a578f28107b1e58358e4b17bd
Eirik Wulff <ew@dibk.no>
2024-08-27 14:11:20 +0200

    $app->configure ikke mulig

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,5 @@ use LaravelZero\Framework\Application;
 
 $app = Application::configure(basePath: dirname(__DIR__))->create();
 $app->register(\Barryvdh\DomPDF\ServiceProvider::class);
-$app->configure('dompdf');
 
 return $app;
