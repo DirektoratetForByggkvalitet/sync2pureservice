b06720cc6e9dac4ca140b9453b67d9b71547fa56
Eirik Wulff <ew@dibk.no>
2024-08-27 14:03:29 +0200

    Fikset manglende oppsett av dompdf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,4 +2,6 @@
 
 use LaravelZero\Framework\Application;
 
+$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
+$app->configure('dompdf');
 return Application::configure(basePath: dirname(__DIR__))->create();
