a5201eaa9fddde7a02202f5f389414783eeaf8ca
Eirik Wulff <ew@dibk.no>
2024-08-27 14:14:53 +0200

    Prøver å flytte serviceprovider til providers.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,7 +2,4 @@
 
 use LaravelZero\Framework\Application;
 
-$app = Application::configure(basePath: dirname(__DIR__))->create();
-$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
-
-return $app;
+return Application::configure(basePath: dirname(__DIR__))->create();
