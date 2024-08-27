d7d848ac3c454bff556fc6fa4856ebc702263707
Eirik Wulff <ew@dibk.no>
2024-08-27 14:38:36 +0200

    La til e-postbiblioteker

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,12 @@
 return [
     App\Providers\AppServiceProvider::class,
     Barryvdh\DomPDF\ServiceProvider::class,
+    Illuminate\Translation\TranslationServiceProvider::class,
+    Illuminate\View\ViewServiceProvider::class,
+    Illuminate\Mail\MailServiceProvider::class,
+    Illuminate\Bus\BusServiceProvider::class,
+    Illuminate\Cache\CacheServiceProvider::class,
+    Illuminate\Routing\RoutingServiceProvider::class,
+    Illuminate\Notifications\NotificationServiceProvider::class,
+    Illuminate\Translation\TranslationServiceProvider::class,
 ];
