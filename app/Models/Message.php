e8138a1bef1a5b77075339915f5313e04ab5b511
Eirik Wulff <ew@dibk.no>
2025-09-09 15:12:39 +0200

    debug: ny splitt

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -404,7 +404,7 @@ class Message extends Model {
         $prosesserteDokumenter = [];
 
         foreach ($dokArray as $dok):
-            $header = explode(' | ', trim(Str::between($dok, 'Saksnr: ', $lf.'  ')));
+            $header = explode(' | ', trim(Str::between($dok, 'Saksnr: ', "\n  ")));
             $saksnr = $header[0];
             $doknr = trim(Str::after($header[1], ':'));
             $sekvensnr = trim(Str::after($header[2], ':'));
