d58d5db4750cafa3922920a76a677643c61faa21
Eirik Wulff <ew@dibk.no>
2025-10-01 11:36:20 +0200

    Slo av sletting av meldinger midlertidig

--- app/Commands/IncomingMessages.php
+++ app/Commands/IncomingMessages.php
@@ -186,7 +186,7 @@ class IncomingMessages extends Command {
             // $bar->advance();
             // Vi har tatt vare på meldingen. Sletter den fra eFormidling sin kø
             if ($deleteMessage && $this->ip->deleteIncomingMessage($message->messageId)):
-                $this->line(Tools::L3.'Meldingen har blitt slettet fra integrasjonspunktet');
+                //$this->line(Tools::L3.'Meldingen har blitt slettet fra integrasjonspunktet');
             else:
                 $this->error(Tools::L3.'Meldingen ble IKKE slettet fra integrasjonspunktet.');
                 if ($this->ps->error_json):
